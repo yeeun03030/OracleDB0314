8c2eef24ea9f7ec94d16d313dec576f6669ec4db
yeeun03030 <skdPdms04@gmail.com>
2024-03-21 10:25:55 +0900

    testSelect()

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -24,5 +25,19 @@ public class MemoRepositoryTests {
             Memo memo = Memo.builder().memoText("Dummy Data Test " + i).build();
             memoRepository.save(memo);
         });
+    }
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L; // Long형에는 L 붙혀야함
+
+        Optional<Memo> result = memoRepository.findById(mno); // Alt+Enter
+
+        System.out.println("====================================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
     }
 }
