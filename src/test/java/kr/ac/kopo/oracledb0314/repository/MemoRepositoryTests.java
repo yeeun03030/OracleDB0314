2aad2eee8de28a932e62e9ee1f5b5456b79009c7
yeeun03030 <skdPdms04@gmail.com>
2024-03-21 11:18:34 +0900

    testSelect2(), testUpdate()

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -4,6 +4,7 @@ import kr.ac.kopo.oracledb0314.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -40,4 +41,27 @@ public class MemoRepositoryTests {
             System.out.println(memo);
         }
     }
+
+    @Transactional
+    @Test
+    public void testSelect2() {
+        Long mno = 100L; // Long형에는 L 붙혀야함
+
+        Memo memo = memoRepository.getOne(mno);// Alt+Enter
+
+        System.out.println("====================================");
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(95L).memoText("Update Dummy Data 95").build();
+
+        Memo memo1 = memoRepository.save(memo);
+
+        System.out.println(memo1);
+    }
+
+
 }
